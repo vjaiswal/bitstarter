4b0b115205c3ad109daca948e49282b2ad9c7e6c
Vivek Jaiswal <vyjaiswal@gmail.com>
2013-07-11 06:45:22 +0000

    Update to web.js: use index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs')
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var data = fs.readFileSync('./index.html', 'utf8')
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
