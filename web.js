38fea7d271d5dc0cb0643e927eacdffe48cedb6e
Vivek Jaiswal <vyjaiswal@gmail.com>
2013-07-11 07:26:43 +0000

    added npn-log and update to web.js

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require('express');
-var fs = require('fs')
+var fs = require('fs');
+var Buffer = require('buffer').Buffer;
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var data = fs.readFileSync('./index.html', 'utf8');
-  var buffer new Buffer[data, 'utf8'];
-  response.send(data);
+  var bufferdata = new Buffer(data);
+  response.send(bufferdata.toString());
 });
 
 var port = process.env.PORT || 5000;
